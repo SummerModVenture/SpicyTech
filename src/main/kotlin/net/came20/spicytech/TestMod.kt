197080675b27214fe37be0d143229c1ec4cdd226
Cameron <camearle20@me.com>
2018-05-22 19:44:46 -0400

    Added submodule maybe?

--- src/main/kotlin/net/came20/spicytech/TestMod.kt
+++ src/main/kotlin/net/came20/spicytech/TestMod.kt
@@ -1,9 +1,11 @@
 package net.came20.spicytech
 
+import net.came20.spicytech.init.ModBlocks
 import net.minecraft.init.Blocks
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.registry.ForgeRegistries
 import org.apache.logging.log4j.Logger
 
 @Mod(modid = ModInfo.MODID, name = ModInfo.NAME, version = ModInfo.VERSION)
@@ -15,10 +17,13 @@ class TestMod {
     @Mod.EventHandler
     fun preInit(e: FMLPreInitializationEvent) {
         logger = e.modLog
+        ModBlocks.init()
     }
 
     @Mod.EventHandler
     fun init(e: FMLInitializationEvent) {
-        logger.info("GRASS BLOCK >> ${Blocks.DIRT.registryName}")
+        ForgeRegistries.ITEMS.keys.forEach {
+            logger.warn(it.resourceDomain + " " + it.resourcePath)
+        }
     }
 }
